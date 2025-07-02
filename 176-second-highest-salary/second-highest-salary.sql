ccca1adbdaf00ad2b0f47d0426d586881786c52c
dhami88 <sandeepdhami943@gmail.com>
2025-07-02 12:54:10 +0530

    Time: 771 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select (select distinct salary from employee e1 where 2=(select count(distinct salary)from employee e2 where e2.salary>=e1.salary))as SecondHighestSalary;
