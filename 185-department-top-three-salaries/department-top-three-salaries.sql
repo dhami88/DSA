bde4be5bd05322e1827317021e32a6da8731a2f5
dhami88 <sandeepdhami943@gmail.com>
2025-07-18 03:20:52 +0530

    Time: 1302 ms (19.90%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,5 +1,5 @@
 Select d.name as department , e1.name as employee, e1.salary as Salary
-From Employee e1 join Department d on e1.DepartmentId = d.Id
+From Employee e1 inner join Department d on e1.DepartmentId = d.Id
 Where  3 >= (select count(distinct (e2.Salary))
             from  Employee e2
             where e2.Salary >= e1.Salary
