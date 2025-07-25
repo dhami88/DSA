c5e519af1738ebe0381cb1d455d01ddf88c5bcf4
dhami88 <sandeepdhami943@gmail.com>
2025-07-25 13:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (86.99%) - LeetSync

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+       int n=nums.size();
+       if(n==1) return nums[0];
+        set<int>st;
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,nums[i]);
+        }
+        if(maxi<0) return maxi;
+        for(int i=0;i<n;i++){
+           if(nums[i]>0) st.insert(nums[i]);
+           else continue;
+        }
+        int ans=0;
+        for(auto it:st){
+            ans+=it;
+        }
+        return ans;
+    }
+};
