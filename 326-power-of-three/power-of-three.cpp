9f8a91f8f10786b952e3d6a9c17c14167637afb2
dhami88 <sandeepdhami943@gmail.com>
2025-08-13 12:13:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (5.34%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -4,7 +4,7 @@ public:
         for(int i=0;i<31;i++){
             if(pow(3,i)==n){
                 return true;
-                break;
+                //break;
             }
         }
         return false;
