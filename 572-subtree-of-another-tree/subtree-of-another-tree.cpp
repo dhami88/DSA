ed7b8f0531fcdc4efea5204f5fe768a2180cc355
dhami88 <sandeepdhami943@gmail.com>
2025-08-15 16:13:03 +0530

    Time: 0 ms (100.00%) | Memory: 29 MB (39.46%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -9,20 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ bool check(TreeNode* root, TreeNode* subroot){
+     if(root==NULL&&subroot==NULL) return true;
+      if(root==NULL||subroot==NULL) return false;
+       return (root->val==subroot->val)&&check(root->right,subroot->right)&&check(root->left,subroot->left);
+ }
 class Solution {
-    public:
-bool isSame(TreeNode* s, TreeNode* t) {
-    if (!s && !t) return true;  // both NULL
-    if (!s || !t) return false; // one NULL, one not
-    return (s->val == t->val) && 
-           isSame(s->left, t->left) && 
-           isSame(s->right, t->right);
-}
-
-bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-    if (!root) return false; // reached end
-    if (isSame(root, subRoot)) return true; // match found here
-    // Otherwise, check left and right
-    return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
-}
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subroot) {
+      if(root==NULL) return false;
+      if(check(root,subroot)) return true;
+      return isSubtree(root->left,subroot)||isSubtree(root->right,subroot);
+    }
 };
