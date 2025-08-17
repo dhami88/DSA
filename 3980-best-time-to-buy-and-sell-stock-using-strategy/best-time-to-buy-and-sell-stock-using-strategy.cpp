67be2782871a91e0ab4be460d19789bd1457e344
dhami88 <sandeepdhami943@gmail.com>
2025-08-17 23:51:28 +0530

    Time: 26 ms (26.28%) | Memory: 328.2 MB (54.45%) - LeetSync

---
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
+        int n=prices.size();
+        vector<long long >p1(n),p2(n);
+        long long sum1=0LL,sum2=0LL;
+        for(int i=0;i<n;i++){
+            sum1+=prices[i];
+            sum2+=(prices[i]*strategy[i]);
+            p1[i]=sum1;
+            p2[i]=sum2;
+        }
+        int j=k-1;
+        long long ans=p2[n-1];
+        while(j<n){
+            int i=(j-k+1);
+            int pos1=j-k;
+            int pos2=j-k/2;
+            long long t = p2[n-1] - (p2[j] - ((pos1 != -1) ? p2[pos1] : 0));
+            t += (p1[j] - ((pos2 != -1) ? p1[pos2] : 0));
+
+            ans=max(ans,t);  
+            j++;
+        }
+        return ans;
+    }
+};
