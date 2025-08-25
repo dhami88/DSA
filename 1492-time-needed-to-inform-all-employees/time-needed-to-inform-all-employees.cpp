ba8361c16de3bb08fa876055dd290b2d8e57a2b5
dhami88 <sandeepdhami943@gmail.com>
2025-08-25 22:53:00 +0530

    Time: 103 ms (79.95%) | Memory: 131.3 MB (52.48%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -1,20 +1,22 @@
 class Solution {
-    int dfs(int headID,vector<vector<int>>&adj,vector<int>& informTime,int curtime){
-        int maxtime=curtime;
-    //     if (adj[headID].empty())
-    // return curtime;
-        for(auto it:adj[headID]){
-           maxtime=max(maxtime, dfs(it,adj,informTime,curtime+informTime[headID]));
-        }
-        return maxtime;
-    }
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         vector<vector<int>>adj(n);
+        queue<pair<int,int>>q;
         for(int i=0;i<n;i++){
-           if(manager[i]!=-1) adj[manager[i]].push_back(i);
+           if(manager[i]!=-1) adj[manager[i]].push_back(i); 
         }
-        return dfs(headID,adj,informTime,0);
-
+        q.push({headID,0});
+        int maxi=0;
+        while(!q.empty()){
+            int node=q.front().first;
+            int time=q.front().second;
+            maxi=max(maxi,time);
+            q.pop();
+            for(auto it:adj[node]){
+                q.push({it,time+informTime[node]});
+            }
+        }
+        return maxi;
     }
 };
