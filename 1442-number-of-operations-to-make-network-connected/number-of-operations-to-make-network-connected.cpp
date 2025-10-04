6138467712c8e7823c7113a8c0f11100e0af6719
dhami88 <sandeepdhami943@gmail.com>
2025-10-04 18:09:39 +0530

    Time: 22 ms (45.24%) | Memory: 46.8 MB (39.35%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,31 +1,53 @@
-class Solution {
-    void dfs(int i,vector<vector<int>>& connections,vector<int>&vis,     vector<vector<int>>&adj){
-        vis[i]=1;
-        for(auto it:adj[i]){
-            if(!vis[it]){
-                dfs(it,connections,vis,adj);
-            }
+class disjointset{
+    public:
+    vector<int>parent,size;
+    public:
+    disjointset(int n){
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            size[i]=1;
         }
+    }
+    int findupar(int node){
+        if(node==parent[node]) return node;
+        else return parent[node]=findupar(parent[node]);
 
     }
+    void unionbysize(int u,int v){
+        int ulp_u=findupar(u);
+        int ulp_v=findupar(v);
+        if(ulp_u==ulp_v) return ;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+                    }
+         else{
+          parent[ulp_v]=ulp_u;
+           size[ulp_u]+=size[ulp_v];
+                    }
+    }
+};
+
+class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-     int m=connections.size();
-     if(m<n-1) return -1;
-     vector<vector<int>>adj(n);
-     for(auto it:connections){
-        int u=it[0];
-        int v=it[1];
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-     }
-     vector<int>vis(n,0);
-     int cnt=0;
-     for(int i=0;i<n;i++){
-    if(!vis[i]) {dfs(i,connections,vis,adj);
-     cnt++;
-    }
-     }
-    return cnt-1;
+        //total number of components-1
+        disjointset ds(n);
+        for(auto it:connections){
+            int u=it[0];
+            int v=it[1];
+            int ulp_u=ds.findupar(u);
+            int ulp_v=ds.findupar(v);
+            if(ulp_u==ulp_v) continue;
+            else ds.unionbysize(u,v);
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(ds.parent[i]==i) cnt++;
+        }
+        if(connections.size()<n-1) return -1;
+        return cnt-1;
     }
 };
