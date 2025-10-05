aa3fadba13facc31be17f5031fa29081e36d01e2
dhami88 <sandeepdhami943@gmail.com>
2025-10-05 19:56:20 +0530

    Time: 14 ms (78.09%) | Memory: 26.3 MB (29.38%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,32 +2,29 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
+        if(n==1&&grid[0][0]==0) return 1;
+        if(grid[0][0]==1||grid[n-1][n-1]==1) return -1;
         queue<pair<int,pair<int,int>>>q;
         vector<vector<int>>dist(n,vector<int>(n,1e9));
-        if(n==1&&grid[0][0]==1) return -1;
-        if(grid[0][0]==1||grid[n-1][n-1]==1) return -1;
-        q.push({1,{0,0}});
-        dist[0][0]=1;
+        dist[0][0]=0;
+        q.push({0,{0,0}});
         while(!q.empty()){
-            int dis=q.front().first;
             int r=q.front().second.first;
             int c=q.front().second.second;
+            int wt=q.front().first;
             q.pop();
             for(int i=-1;i<=1;i++){
                 for(int j=-1;j<=1;j++){
                     int nr=r+i;
                     int nc=c+j;
-                    if(nr>=0&&nr<n&&nc>=0&&nc<n&&grid[nr][nc]==0){
-                        if(dis+1<dist[nr][nc]){
-                        q.push({dis+1,{nr,nc}});
-                        dist[nr][nc]=1+dis;
-                        }
-                        if(nr==n-1&&nc==n-1) return dist[nr][nc];
+                    if(nr>=0&&nr<n&&nc>=0&&nc<n&&grid[nr][nc]==0&&1+wt<dist[nr][nc]){
+                        dist[nr][nc]=1+wt;
+                        q.push({dist[nr][nc],{nr,nc}});
+                        if(nr==n-1&&nc==n-1) return 1+dist[nr][nc];
                     }
                 }
             }
         }
-
-      return -1;
+        return -1;
     }
 };
