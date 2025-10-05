4a23f2d9b0100d23e78db23682e7b758f8516fad
dhami88 <sandeepdhami943@gmail.com>
2025-10-05 18:43:56 +0530

    Time: 82 ms (65.12%) | Memory: 48.1 MB (20.28%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int>dist(n,1e9);
-        priority_queue<pair<int,int>,
-        vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        vector<vector<pair<int,int>>>adj(n);
-        for(auto it:times){
-            int u=it[0]-1;
-            int v=it[1]-1;
-            int t=it[2];
-            adj[u].push_back({v,t});
+        vector<vector<pair<int,int>>>adj(n+1);
+        for (auto it:times){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
+            adj[u].push_back({v,wt});
         }
-        pq.push({0,k-1});
-        dist[k-1]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,k});
+        vector<int>dist(n+1,1e9);
+        dist[k]=0;
         while(!pq.empty()){
+            int wt=pq.top().first;
             int node=pq.top().second;
-            int dis=pq.top().first;
             pq.pop();
             for(auto it:adj[node]){
-                int next=it.first;
-                int d=it.second;
-                if(dist[next]>dis+d){
-                    dist[next]=dis+d;
-                    pq.push({dist[next],next});
+                int dis=it.second;
+                int no=it.first;
+                if(dis+wt<dist[no]){
+                    dist[no]=dis+wt;
+                    pq.push({dist[no],no});
                 }
             }
-       
         }
-        int ans=0;
-        for(int i=0;i<n;i++){
+        int ans=INT_MIN;
+        for(int i=1;i<=n;i++){
+          
             if(dist[i]==1e9) return -1;
-            else {
-                ans=max(ans,dist[i]);
-            }
+             else ans=max(ans,dist[i]);
+            
         }
         return ans;
     }
