3f5461c6ce4751657357e7695590ab997a679d4f
dhami88 <sandeepdhami943@gmail.com>
2025-10-11 11:34:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (98.01%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -5,11 +5,11 @@ public:
       if(st.find(endGene)==st.end()) return -1;
       queue<string>q;
       q.push(startGene);
-      vector<char>gene;
-      gene.push_back('A');
-      gene.push_back('G');
-      gene.push_back('C');
-      gene.push_back('T');
+    //   vector<char>gene;
+    //   gene.push_back('A');
+    //   gene.push_back('G');
+    //   gene.push_back('C');
+    //   gene.push_back('T');
       unordered_set<string>vis;
       vis.insert(startGene);
       int ans=0;
@@ -19,7 +19,7 @@ public:
         auto s=q.front();
         q.pop();
         if(s==endGene) return ans;
-         for(auto ch:gene){
+         for(auto ch:"AGCT"){
             for(int i=0;i<8;i++){
                 string temp=s;
                 if(temp[i]==ch) continue;
