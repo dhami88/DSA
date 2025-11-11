156c3b789d7a347c78c2e30a9523c4bcc14cdf9d
dhami88 <sandeepdhami943@gmail.com>
2025-11-12 01:18:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (47.18%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int minOpen = 0, maxOpen = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                minOpen++;
+                maxOpen++;
+            } else if (c == ')') {
+                minOpen--;
+                maxOpen--;
+            } else { // '*'
+                minOpen--;   // treat '*' as ')'
+                maxOpen++;   // treat '*' as '('
+            }
+
+            if (maxOpen < 0) return false; // too many ')'
+            if (minOpen < 0) minOpen = 0;  // can't go below 0
+        }
+
+        return minOpen == 0;
+    }
+};
