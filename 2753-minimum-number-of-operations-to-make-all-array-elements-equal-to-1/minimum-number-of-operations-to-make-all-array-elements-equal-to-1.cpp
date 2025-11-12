d7593bf8930af410b75d3352f61cd6e62d11f59a
dhami88 <sandeepdhami943@gmail.com>
2025-11-12 18:52:22 +0530

    Time: 0 ms (100.00%) | Memory: 31.6 MB (72.22%) - LeetSync

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -1,23 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int n=nums.size();
-        // min length subarray with gcd=1;
-        //gcd(x,0)=x;
-        int ones=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==1) ones++;
-        }
-        if(ones>0) return n-ones; 
-        int len=n+1;
-        for(int i=0;i<n;i++){
-            int g=0;
-            for(int j=i;j<n;j++){
-                g=gcd(g,nums[j]);
-                if(g==1) len=min(len,j-i+1);
+        int n = nums.size();
+        int g = 0;
+        for (int x : nums) g = gcd(g, x);
+        if (g > 1) return -1;
+
+        int ones = count(nums.begin(), nums.end(), 1);
+        if (ones > 0)
+            return n - ones;
+
+        int minLen = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            int g = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                g = gcd(g, nums[j]);
+                if (g == 1) {
+                    minLen = min(minLen, j - i + 1);
+                    break;
+                }
             }
         }
-        if(len==n+1) return -1;
-        else return len+n-2;;
+        return (minLen - 1) + (n - 1);
     }
 };
