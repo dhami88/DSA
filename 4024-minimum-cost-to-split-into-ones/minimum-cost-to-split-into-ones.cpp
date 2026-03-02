891b534d0b520c7629854f5a93c56bebd19b391c
dhami88 <sandeepdhami943@gmail.com>
2026-03-02 16:18:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (90.00%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int minCost(int n) {
-    vector<int>dp(501,0);
-    dp[1]=0;
-    for(int i=2;i<=500;i++){
-        dp[i]=dp[i-1]+i-1;
-    }
-    return dp[n];
+    
+    return (n*(n-1))/2;
     }
 };
