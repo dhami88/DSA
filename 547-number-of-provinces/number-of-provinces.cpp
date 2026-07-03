883b55356daa8567c07d493084aff3fb65c902d8
dhami88 <sandeepdhami943@gmail.com>
2026-07-04 00:24:59 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (16.54%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,55 +1,33 @@
-class disjointset{
-   vector<int>parent,size;
-   public:
-   disjointset(int n){
-    parent.resize(n+1);
-    size.resize(n+1);
-    for(int i=0;i<n;i++){
-        parent[i]=i;
-        size[i]=1;
-    }
-   }
-   int findupar(int node){
-    if (parent[node]==node) return node;
-    else return parent[node]=findupar(parent[node]);
-   }
-   void unionbysize(int u,int v){
-    int ulp_u=findupar(u);
-    int ulp_v=findupar(v);
-    if(ulp_u==ulp_v) return;
-    if(size[ulp_u]<size[ulp_v]){
-        parent[ulp_u]=ulp_v;
-        size[ulp_v]+=size[ulp_u];
-
-    }
-    else{
-        parent[ulp_v]=ulp_u;
-        size[ulp_u]+=size[ulp_v];
-    }
-
-   }
-
-
-
-};
-
 class Solution {
-public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        // Using disjoint set union
-        int n=isConnected.size();
-        disjointset ds(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1&&i!=j){
-                    ds.unionbysize(i,j);
-                }
+    void dfs(int node,vector<vector<int>>&adj,vector<int>&vis){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                dfs(it,adj,vis);
             }
         }
-        int cnt=0;
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<vector<int>>adj(n);
         for(int i=0;i<n;i++){
-            if(ds.findupar(i)==i) cnt++;
+            for(int j=0;j<n;j++){
+            if(isConnected[i][j]==1 && i!=j){
+                adj[i].push_back(j);
+                adj[j].push_back(i);
+            }
+            }
         }
-        return cnt;
+            int ans =0;
+            vector<int>vis(n,0);
+            for(int i = 0;i<n;i++){
+              if(!vis[i]){
+                ans++;
+                dfs(i,adj,vis);
+              }
+            }
+        
+        return ans;
     }
 };
